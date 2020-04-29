74432a9b94570c5663cb72f95609b73a1b1b975d
Viktor G. Gumennyy <20798349+sairus7@users.noreply.github.com>
2020-04-30 02:10:27 +0300

    Files generated by PkgTemplates

---
+++ src/SortFilters.jl
@@ -0,0 +1,5 @@
+module SortFilters
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SortFilters
+using Test
+
+@testset "SortFilters.jl" begin
+    # Write your own tests here.
+end
